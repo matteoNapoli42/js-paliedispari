95b62cfc9d89129b2aedf91314d2295d3ca7dd29
matteoNapoli42 <matteon4242@gmail.com>
2023-08-31 20:29:20 +0200

    finished first part

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,43 @@
 /*Pari e Dispari*/
+let scelta=document.getElementById("scelta").value;
+let flag=false;
+let pcNumber=0;
+let somma=0;
 
+
+function genPcNumber() 
+{
+    pcNumber=Math.floor(Math.random()*6);
+    console.log(pcNumber);
+}
+
+
+function showResults()
+{
+    let element = document.getElementById("esitoCard");
+    let message = document.getElementById("message");
+    let userNumber = parseInt(document.getElementById("userNumber").value);
+    console.log(userNumber);
     
+    if(userNumber<1 || userNumber>5)
+    {
+        element.classList.add("d-none");
+        message.innerHTML = "Il numero inserito non è valido. Riprovare";
+        return null;
+    }
+    else
+    {
+        element.classList.remove("d-none");
+        message.innerHTML = "ESITO";
+        console.log(pcNumber);
+        somma = userNumber + pcNumber;
+        let printSomma = document.getElementById("somma");
+        printSomma.innerHTML = somma;
 
+    }
+}
+
+
+   
 
 
